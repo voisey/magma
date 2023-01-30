6ba6d6801d39a7adcd70f20846308fb2c407d98b
Sebastian Wolf <sebastian.wolf@tngtech.com>
2023-01-30 14:53:10 +1100

    fix(feg): typo in feg fabfile import
    Signed-off-by: Sebastian Wolf <sebastian.wolf@tngtech.com>

--- feg/gateway/fabfile.py
+++ feg/gateway/fabfile.py
@@ -19,7 +19,7 @@ sys.path.append('../../orc8r')
 import tools.fab.dev_utils as dev_utils  # NOQA
 import tools.fab.types as types
 from fabric import Connection, task
-from tools.fab.host import vagrant_connection
+from tools.fab.hosts import vagrant_connection
 
 SNOWFLAKE_FEG_FILE = '../../.cache/feg/snowflake'
 NETWORK_ID = 'feg_test'
